4fe041b3f1634a6356438219d51074df87336a18
ajav06 <albert06av@hotmail.com>
2019-01-10 19:25:40 -0400

    Herencia

--- src/modelo/Paciente.java
+++ src/modelo/Paciente.java
@@ -1,5 +1,45 @@
 package modelo;
+import java.util.Date;
 
-public class Paciente {
-
+public class Paciente extends Persona{
+	String antePersonales, anteFamiliares;
+	int nroSeguro,nroHistorial;
+	
+	public Paciente(String cedula, String nombre, String apellido, String antePersonales, String anteFamiliares, int nroSeguro, int nroHistorial, Date fechaNacimiento) {
+		super();
+		this.cedula=cedula;
+		this.nombre=nombre;
+		this.apellido=apellido;
+		this.antePersonales=antePersonales;
+		this.anteFamiliares=anteFamiliares;
+		this.nroSeguro=nroSeguro;
+		this.nroHistorial=nroHistorial;
+		this.fechaNacimiento=fechaNacimiento;
+	}
+	
+	public String getAntePersonales() {
+		return antePersonales;
+	}
+	public void setAntePersonales(String antePersonales) {
+		this.antePersonales = antePersonales;
+	}
+	public String getAnteFamiliares() {
+		return anteFamiliares;
+	}
+	public void setAnteFamiliares(String anteFamiliares) {
+		this.anteFamiliares = anteFamiliares;
+	}
+	public int getNroSeguro() {
+		return nroSeguro;
+	}
+	public void setNroSeguro(int nroSeguro) {
+		this.nroSeguro = nroSeguro;
+	}
+	public int getNroHistorial() {
+		return nroHistorial;
+	}
+	public void setNroHistorial(int nroHistorial) {
+		this.nroHistorial = nroHistorial;
+	}
+	
 }
---
+++ src/modelo/Persona.java
@@ -0,0 +1,76 @@
+package modelo;
+import java.util.Date;
+
+public class Persona {
+	String cedula, nombre, apellido, direccion;
+	int nroTelefonico;
+	float peso;
+	Date fechaNacimiento;
+	
+	public Persona() {
+		super();
+		this.cedula="";
+		this.nombre="";
+		this.apellido="";
+		this.direccion="";
+		this.peso=0;
+	}
+		
+	public String getCedula() {
+		return cedula;
+	}
+	
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellido() {
+		return apellido;
+	}
+	
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public int getNroTelefonico() {
+		return nroTelefonico;
+	}
+	
+	public void setNroTelefonico(int nroTelefonico) {
+		this.nroTelefonico = nroTelefonico;
+	}
+	
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	
+	
+}
