c59e6354abdadf9b3cba60fa5283e82595318314
ajav06 <albert06av@hotmail.com>
2019-01-10 23:45:53 -0400

    Mejora

--- src/vista/VentanaEliminarPaciente.java
+++ src/vista/VentanaEliminarPaciente.java
@@ -15,12 +15,12 @@ import javax.swing.JScrollPane;
 import javax.swing.JButton;
 
 public class VentanaEliminarPaciente extends javax.swing.JFrame{
-	private JTextField textField_Ced;
-	private JTextField textField_FechaN;
-	private JTextField textField_Nomb;
-	private JTextField textField_Apelli;
 	private JTextField textField_NroHist;
 	private JTextField textField_NroSeguro;
+	private JTextField textField_Ced;
+	private JTextField textField_Nomb;
+	private JTextField textField_Apelli;
+	private JTextField textField_FechaN;
 	public VentanaEliminarPaciente () {
 		getContentPane().setForeground(Color.BLUE);
 		
@@ -30,90 +30,7 @@ public class VentanaEliminarPaciente extends javax.swing.JFrame{
 		lblEliminarP.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JPanel panel_DMed = new JPanel();
-		panel_DMed.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0)), "Datos Personales", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		
-		JLabel lblCed = new JLabel("Cedula : ");
-		
-		JLabel lblFechaN = new JLabel("Fecha Nac. :");
-		
-		textField_Ced = new JTextField();
-		textField_Ced.setEditable(false);
-		textField_Ced.setColumns(10);
-		
-		textField_FechaN = new JTextField();
-		textField_FechaN.setEditable(false);
-		textField_FechaN.setColumns(10);
-		
-		JLabel lblNomb = new JLabel("Nombres :");
-		
-		textField_Nomb = new JTextField();
-		textField_Nomb.setEditable(false);
-		textField_Nomb.setColumns(10);
-		
-		JLabel lblApelli = new JLabel("Apellidos :");
-		
-		textField_Apelli = new JTextField();
-		textField_Apelli.setEditable(false);
-		textField_Apelli.setColumns(10);
-		GroupLayout gl_panel_DMed = new GroupLayout(panel_DMed);
-		gl_panel_DMed.setHorizontalGroup(
-			gl_panel_DMed.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_DMed.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_panel_DMed.createParallelGroup(Alignment.TRAILING)
-						.addComponent(lblNomb, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addGroup(gl_panel_DMed.createSequentialGroup()
-							.addGap(6)
-							.addComponent(lblCed, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-					.addGap(18)
-					.addGroup(gl_panel_DMed.createParallelGroup(Alignment.LEADING)
-						.addComponent(textField_Ced, GroupLayout.DEFAULT_SIZE, 107, Short.MAX_VALUE)
-						.addComponent(textField_Nomb, GroupLayout.DEFAULT_SIZE, 107, Short.MAX_VALUE))
-					.addGap(44)
-					.addGroup(gl_panel_DMed.createParallelGroup(Alignment.TRAILING)
-						.addComponent(lblFechaN, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addGroup(gl_panel_DMed.createSequentialGroup()
-							.addGap(12)
-							.addComponent(lblApelli, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-					.addGap(18)
-					.addGroup(gl_panel_DMed.createParallelGroup(Alignment.LEADING)
-						.addComponent(textField_Apelli, GroupLayout.DEFAULT_SIZE, 115, Short.MAX_VALUE)
-						.addComponent(textField_FechaN, GroupLayout.DEFAULT_SIZE, 115, Short.MAX_VALUE))
-					.addContainerGap())
-		);
-		gl_panel_DMed.setVerticalGroup(
-			gl_panel_DMed.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_DMed.createSequentialGroup()
-					.addGap(19)
-					.addGroup(gl_panel_DMed.createParallelGroup(Alignment.TRAILING)
-						.addGroup(gl_panel_DMed.createParallelGroup(Alignment.BASELINE)
-							.addComponent(textField_FechaN, GroupLayout.DEFAULT_SIZE, 20, Short.MAX_VALUE)
-							.addGroup(gl_panel_DMed.createSequentialGroup()
-								.addGap(6)
-								.addComponent(lblFechaN, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-						.addGroup(gl_panel_DMed.createParallelGroup(Alignment.BASELINE)
-							.addGroup(gl_panel_DMed.createSequentialGroup()
-								.addGap(6)
-								.addComponent(lblCed, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-							.addComponent(textField_Ced)))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_panel_DMed.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel_DMed.createParallelGroup(Alignment.BASELINE)
-							.addGroup(gl_panel_DMed.createSequentialGroup()
-								.addGap(6)
-								.addComponent(lblNomb, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-							.addComponent(textField_Nomb))
-						.addGroup(gl_panel_DMed.createParallelGroup(Alignment.BASELINE)
-							.addComponent(textField_Apelli)
-							.addGroup(gl_panel_DMed.createSequentialGroup()
-								.addGap(6)
-								.addComponent(lblApelli, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
-					.addGap(17))
-		);
-		panel_DMed.setLayout(gl_panel_DMed);
-		
-		JPanel panel_DPer = new JPanel();
-		panel_DPer.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0)), "Datos Medicos", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_DMed.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0)), "Datos Medicos", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		
 		JLabel lblNroHist = new JLabel("Nro. Historial :");
 		
@@ -134,47 +51,47 @@ public class VentanaEliminarPaciente extends javax.swing.JFrame{
 		JScrollPane scrollPane_1 = new JScrollPane();
 		
 		JScrollPane scrollPane_2 = new JScrollPane();
-		GroupLayout gl_panel_DPer = new GroupLayout(panel_DPer);
-		gl_panel_DPer.setHorizontalGroup(
-			gl_panel_DPer.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_DPer.createSequentialGroup()
+		GroupLayout gl_panel_DMed = new GroupLayout(panel_DMed);
+		gl_panel_DMed.setHorizontalGroup(
+			gl_panel_DMed.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_DMed.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_panel_DPer.createParallelGroup(Alignment.TRAILING)
-						.addGroup(gl_panel_DPer.createSequentialGroup()
+					.addGroup(gl_panel_DMed.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_panel_DMed.createSequentialGroup()
 							.addComponent(lblNroHist, GroupLayout.DEFAULT_SIZE, 69, Short.MAX_VALUE)
 							.addPreferredGap(ComponentPlacement.RELATED)
 							.addComponent(textField_NroHist, GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE))
 						.addComponent(lblAntecedentesPersonales, GroupLayout.DEFAULT_SIZE, 181, Short.MAX_VALUE)
 						.addComponent(scrollPane_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 181, Short.MAX_VALUE))
 					.addGap(35)
-					.addGroup(gl_panel_DPer.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel_DPer.createSequentialGroup()
+					.addGroup(gl_panel_DMed.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_DMed.createSequentialGroup()
 							.addComponent(lblAntecedentesFamiliares, GroupLayout.DEFAULT_SIZE, 129, Short.MAX_VALUE)
 							.addGap(69))
-						.addGroup(gl_panel_DPer.createSequentialGroup()
+						.addGroup(gl_panel_DMed.createSequentialGroup()
 							.addComponent(lblNroSeguro, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 							.addGap(18)
 							.addComponent(textField_NroSeguro, GroupLayout.DEFAULT_SIZE, 115, Short.MAX_VALUE))
 						.addComponent(scrollPane_2, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE))
 					.addContainerGap())
 		);
-		gl_panel_DPer.setVerticalGroup(
-			gl_panel_DPer.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_DPer.createSequentialGroup()
+		gl_panel_DMed.setVerticalGroup(
+			gl_panel_DMed.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_DMed.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_panel_DPer.createParallelGroup(Alignment.TRAILING)
-						.addGroup(gl_panel_DPer.createParallelGroup(Alignment.BASELINE)
+					.addGroup(gl_panel_DMed.createParallelGroup(Alignment.TRAILING)
+						.addGroup(gl_panel_DMed.createParallelGroup(Alignment.BASELINE)
 							.addComponent(textField_NroHist, GroupLayout.DEFAULT_SIZE, 20, Short.MAX_VALUE)
 							.addComponent(lblNroHist, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-						.addGroup(gl_panel_DPer.createParallelGroup(Alignment.BASELINE)
+						.addGroup(gl_panel_DMed.createParallelGroup(Alignment.BASELINE)
 							.addComponent(lblNroSeguro, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 							.addComponent(textField_NroSeguro, GroupLayout.DEFAULT_SIZE, 20, Short.MAX_VALUE)))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_panel_DPer.createParallelGroup(Alignment.BASELINE)
+					.addGroup(gl_panel_DMed.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblAntecedentesPersonales, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 						.addComponent(lblAntecedentesFamiliares, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_panel_DPer.createParallelGroup(Alignment.TRAILING)
+					.addGroup(gl_panel_DMed.createParallelGroup(Alignment.TRAILING)
 						.addComponent(scrollPane_2, GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE)
 						.addComponent(scrollPane_1, GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE))
 					.addGap(18))
@@ -187,7 +104,7 @@ public class VentanaEliminarPaciente extends javax.swing.JFrame{
 		JTextPane textPane_AntPer = new JTextPane();
 		textPane_AntPer.setEditable(false);
 		scrollPane_1.setViewportView(textPane_AntPer);
-		panel_DPer.setLayout(gl_panel_DPer);
+		panel_DMed.setLayout(gl_panel_DMed);
 		
 		JButton btnCancelar = new JButton("No");
 		btnCancelar.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
@@ -200,42 +117,124 @@ public class VentanaEliminarPaciente extends javax.swing.JFrame{
 		
 		JLabel lblseguroQueDesea = new JLabel("\u00BFSeguro que desea Eliminarlo?");
 		lblseguroQueDesea.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 13));
+		
+		JPanel panel_DPer = new JPanel();
+		panel_DPer.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0)), "Datos Personales", TitledBorder.LEFT, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		
+		JLabel lblNomb = new JLabel("Nombres :");
+		
+		JLabel lblCed = new JLabel("Cedula : ");
+		
+		textField_Ced = new JTextField();
+		textField_Ced.setEditable(false);
+		textField_Ced.setColumns(10);
+		
+		textField_Nomb = new JTextField();
+		textField_Nomb.setEditable(false);
+		textField_Nomb.setColumns(10);
+		
+		JLabel lblFechaN = new JLabel("Fecha Nac. :");
+		
+		JLabel lblApelli = new JLabel("Apellidos :");
+		
+		textField_Apelli = new JTextField();
+		textField_Apelli.setEditable(false);
+		textField_Apelli.setColumns(10);
+		
+		textField_FechaN = new JTextField();
+		textField_FechaN.setEditable(false);
+		textField_FechaN.setColumns(10);
+		GroupLayout gl_panel_DPer = new GroupLayout(panel_DPer);
+		gl_panel_DPer.setHorizontalGroup(
+			gl_panel_DPer.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 448, Short.MAX_VALUE)
+				.addGroup(gl_panel_DPer.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_DPer.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lblNomb, GroupLayout.DEFAULT_SIZE, 52, Short.MAX_VALUE)
+						.addGroup(gl_panel_DPer.createSequentialGroup()
+							.addGap(6)
+							.addComponent(lblCed, GroupLayout.DEFAULT_SIZE, 46, Short.MAX_VALUE)))
+					.addGap(18)
+					.addGroup(gl_panel_DPer.createParallelGroup(Alignment.LEADING)
+						.addComponent(textField_Ced, GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE)
+						.addComponent(textField_Nomb, GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE))
+					.addGap(44)
+					.addGroup(gl_panel_DPer.createParallelGroup(Alignment.TRAILING)
+						.addComponent(lblFechaN, GroupLayout.DEFAULT_SIZE, 62, Short.MAX_VALUE)
+						.addGroup(gl_panel_DPer.createSequentialGroup()
+							.addGap(12)
+							.addComponent(lblApelli, GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)))
+					.addGap(18)
+					.addGroup(gl_panel_DPer.createParallelGroup(Alignment.LEADING)
+						.addComponent(textField_Apelli, GroupLayout.DEFAULT_SIZE, 116, Short.MAX_VALUE)
+						.addComponent(textField_FechaN, GroupLayout.DEFAULT_SIZE, 116, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		gl_panel_DPer.setVerticalGroup(
+			gl_panel_DPer.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 103, Short.MAX_VALUE)
+				.addGroup(gl_panel_DPer.createSequentialGroup()
+					.addGap(19)
+					.addGroup(gl_panel_DPer.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_DPer.createSequentialGroup()
+							.addGap(6)
+							.addComponent(lblFechaN, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+						.addComponent(textField_FechaN, GroupLayout.DEFAULT_SIZE, 20, Short.MAX_VALUE)
+						.addGroup(gl_panel_DPer.createSequentialGroup()
+							.addGap(6)
+							.addComponent(lblCed, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+						.addComponent(textField_Ced))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel_DPer.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_DPer.createSequentialGroup()
+							.addGap(6)
+							.addComponent(lblNomb, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+						.addComponent(textField_Nomb)
+						.addGroup(gl_panel_DPer.createSequentialGroup()
+							.addGap(6)
+							.addComponent(lblApelli, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+						.addComponent(textField_Apelli))
+					.addGap(17))
+		);
+		panel_DPer.setLayout(gl_panel_DPer);
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 					.addGap(27)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-							.addComponent(panel_DPer, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addGroup(groupLayout.createSequentialGroup()
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(panel_DPer, GroupLayout.DEFAULT_SIZE, 436, Short.MAX_VALUE)
+							.addGap(22))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 								.addComponent(panel_DMed, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addGap(2)))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(15)
-							.addComponent(btnCancelar, GroupLayout.DEFAULT_SIZE, 176, Short.MAX_VALUE)
-							.addGap(43)
-							.addComponent(btnAceptar, GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE)
-							.addGap(21))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(83)
-							.addComponent(lblEliminarP, GroupLayout.DEFAULT_SIZE, 269, Short.MAX_VALUE)
-							.addGap(92))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(124)
-							.addComponent(lblseguroQueDesea, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addGap(125)))
-					.addGap(26))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(15)
+									.addComponent(btnCancelar, GroupLayout.DEFAULT_SIZE, 176, Short.MAX_VALUE)
+									.addGap(43)
+									.addComponent(btnAceptar, GroupLayout.DEFAULT_SIZE, 189, Short.MAX_VALUE)
+									.addGap(21))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(83)
+									.addComponent(lblEliminarP, GroupLayout.DEFAULT_SIZE, 269, Short.MAX_VALUE)
+									.addGap(92))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(124)
+									.addComponent(lblseguroQueDesea, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addGap(125)))
+							.addGap(26))))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(lblEliminarP, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-					.addGap(26)
-					.addComponent(panel_DMed, GroupLayout.DEFAULT_SIZE, 103, Short.MAX_VALUE)
 					.addGap(28)
-					.addComponent(panel_DPer, GroupLayout.DEFAULT_SIZE, 237, Short.MAX_VALUE)
+					.addComponent(panel_DPer, GroupLayout.DEFAULT_SIZE, 103, Short.MAX_VALUE)
+					.addGap(26)
+					.addComponent(panel_DMed, GroupLayout.DEFAULT_SIZE, 237, Short.MAX_VALUE)
 					.addGap(18)
 					.addComponent(lblseguroQueDesea, GroupLayout.DEFAULT_SIZE, 16, Short.MAX_VALUE)
 					.addGap(11)
