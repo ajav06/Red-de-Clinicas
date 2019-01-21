67ab739f22b3fd46f23570e6b1e035bfa2db5fe6
Marielbamar21 <marielbamaldonado20@gmail.com>
2019-01-20 23:58:05 -0400

    casi

--- src/contrilador/Clinica/ControladorElimClinica.java
+++ src/contrilador/Clinica/ControladorElimClinica.java
@@ -43,4 +43,4 @@ public class ControladorElimClinica implements ActionListener
 		}
 	}
 
-}
+
--- src/contrilador/Clinica/ControladorVtnActuClinica.java
+++ src/contrilador/Clinica/ControladorVtnActuClinica.java
@@ -38,15 +38,14 @@ public class ControladorVtnActuClinica implements ActionListener
 	    	if(vtnActuCli.getCodigo().equals("") || vtnActuCli.getNombre().equals("") || vtnActuCli.getEstado().equals("") 
 	    			|| vtnActuCli.getCiudad().equals("") || vtnActuCli.getUbicacion().equals("")|| vtnActuCli.getTelefono().equals("")|| vtnActuCli.getCorreo().equals("") )
 	    		 //Deben estar todos los campos llenos para poder actualizar al paciente
-	    	//	vtnActuCli.mostrarMensaje("Debe llenar todos los datos para poder actualizar al paciente");
+	    	vtnActuCli.mostrarMensaje("Debe llenar todos los datos para poder actualizar al paciente");
 	    		else
 	    	{
 	    		ClinicaBD clinicaBD = new ClinicaBD();
 		    	Clinica clinica = new Clinica(vtnActuCli.getCodigo(),vtnActuCli.getNombre(),
 		    		vtnActuCli.getEstado(),vtnActuCli.getCiudad(),vtnActuCli.getUbicacion(),
 		    		vtnActuCli.getTelefono(),vtnActuCli.getCorreo());		    
-		    	ClinicaBD.a);
-		    //	vtnActuCli.mostrarMensaje("El Paciente fue incluido con exito");
+		       vtnActuCli.mostrarMensaje("El Paciente fue incluido con exito");
 		    	vtnActuCli.blanquearCampos();
 	    	}
 		}catch(Exception e)
